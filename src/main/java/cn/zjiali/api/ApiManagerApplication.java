fa7d83659d7fd572eef373e212df76656aac423e
Gary <cnahzjl@gmail.com>
2021-12-03 22:58:39 +0800

    添加数据源,集成mybatis-plus

--- src/main/java/cn/zjiali/api/ApiManagerApplication.java
+++ src/main/java/cn/zjiali/api/ApiManagerApplication.java
@@ -1,10 +1,12 @@
 package cn.zjiali.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@MapperScan(basePackages = {"cn.zjiali.api.mapper"})
 public class ApiManagerApplication {
 
     public static void main(String[] args) {

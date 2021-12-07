c3eb75403151359df1958566ac2b62391b860766
Gary <cnahzjl@gmail.com>
2021-12-07 13:50:48 +0800

    删除多余引入

--- src/main/java/cn/zjiali/api/ApiManagerApplication.java
+++ src/main/java/cn/zjiali/api/ApiManagerApplication.java
@@ -3,7 +3,6 @@ package cn.zjiali.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication

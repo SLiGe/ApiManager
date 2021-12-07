6a374e948c330bbea4e00495ee854384d08557ad
Gary <cnahzjl@gmail.com>
2021-12-07 13:50:26 +0800

    调整静态资源

--- src/main/java/cn/zjiali/api/controller/IndexController.java
+++ src/main/java/cn/zjiali/api/controller/IndexController.java
@@ -2,23 +2,30 @@ package cn.zjiali.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author zJiaLi
  * @since 2021-12-04 11:17
  */
 @Controller
-public class IndexController {
+public class IndexController  {
 
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
     }
+
+    @GetMapping(value = {"/", "index.html"})
+    public String index() {
+        return "index";
+    }
+
 
 }
